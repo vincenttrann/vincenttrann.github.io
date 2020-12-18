32d9f9f9d29816139b6c265ae6ccf9dec5d7f547
vincenttrann <vincenttran4644@gmail.com>
2020-12-17 17:50:16 -0800

    Portfolio v2

---
+++ readme.txt
@@ -0,0 +1,34 @@
+Vincent Tran, vincent6@uci.edu
+
+1. How to run the project.
+Simply access it online through https://vincenttrann.github.io/ or open the code and run it through a live server.
+
+
+2. What online resources did you consult when completing this assignment? (list specific URLs)
+https://getbootstrap.com/docs/4.3/layout/grid/
+https://www.internetingishard.com/
+https://getbootstrap.com/docs/4.5/getting-started/introduction/
+https://developer.mozilla.org/en-US/
+https://stackoverflow.com/questions/54404865/make-bootstrap-card-entirely-clickable
+https://inf133-fa20.markbaldw.in/resources.html
+https://inf133-fa20.markbaldw.in/notes/wk1.html
+https://inf133-fa20.markbaldw.in/notes/wk2.html
+http://www.codingeverything.com/2014/02/BootstrapDocsSideBar.html
+https://www.youtube.com/watch?v=FVRfM-2Q4JQ
+https://www.developerdrive.com/8-simple-css-hover-effects/
+https://stackoverflow.com/questions/7546389/css-overflow-only-1-line-of-text
+https://fontawesome.com/icons/angle-down
+https://getbootstrap.com/docs/4.0/layout/overview/
+https://stackoverflow.com/questions/8188060/how-to-make-link-not-change-color-after-visited
+https://www.w3schools.com/js/js_random.asp
+https://stackoverflow.com/questions/51562340/css-background-fills-on-hover-from-bottom-to-top
+https://jsfiddle.net/gableroux/S2SMK/
+https://stackoverflow.com/questions/27610892/highlight-menu-on-scrolling-if-reach-div
+https://stackoverflow.com/questions/47436186/highlight-text-on-scroll
+https://github.com/michalsnik/aos/blob/next/README.md
+https://itnext.io/how-to-use-animate-on-scroll-library-218407e041db
+https://getbootstrap.com/docs/4.0/components/collapse/
+https://labs.danielcardoso.net/load-awesome/animations/line-scale.html
+
+3. Bonus
+I utilized a "animate on scroll" library to add more appealing animations to a otherwise static page.
---
+++ script.js
@@ -0,0 +1,144 @@
+
+$(document).ready(function(){
+  //Fun facts
+  const funFacts = [
+    "I am 6'3 without shoes.",
+    "I love playing basketball and volleyball!",
+    "I am a huge video game nerd.",
+    "I mostly listen to KPOP and EDM.",
+    "It is my goal to retire as early as possible.",
+    "I love learning about new technologies.",
+    "Product design is my career passion.",
+    "I follow the NBA religiously.",
+    "I've been a Los Angeles Laker fan since I was born.",
+    "I currently play VALORANT and my peak rank was Diamond!",
+    "I'm affiliated with the professinal business frat: Alpha Kappa Psi.",
+    "My favorite dessert is jasmine milk tea with boba.",
+    "I once gained 25 pounds of muscle over a summer."
+  ];
+  //Random fact generator
+  var randomFactIndex = Math.floor(Math.random() * funFacts.length);
+  $('#preloader-title').text("Vincent's Fun Fact #"+(randomFactIndex+1));
+  $('#preloader-content').html(funFacts[randomFactIndex]);
+  //Load preloaders
+  $( window ).on( "load", function() {
+    preloaderFadeOutTime = 800;
+    var preloader = $('.spinner-wrapper');
+    preloader.animate({
+      up:'40px',
+      opacity:0
+    },preloaderFadeOutTime);
+    preloader.css("visibility","hidden");
+    AOS.init();
+  });
+
+  //Scroll to reveal
+  $(window).scroll(function(){
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 20);
+
+    $(".highlight").each(function(){
+            if ( $(this).isOnScreenHighlight() ) {
+            $(this).addClass('shown');
+                } else {
+                  $(this).removeClass('shown');
+            }
+    });
+  });
+
+  //Detail button
+  $(".p-center-wrapper button[data-toggle='collapse']").click (function () {
+    $(this).text(function(i,old){
+      var newString = "";
+      if (old.startsWith("See")) {
+        newString = old.replace("See","Hide");
+      } else if (old.startsWith("Hide")){
+        newString = old.replace ("Hide","See");
+      } else {
+        newString = old;
+      }
+      return newString;
+    });
+  })
+
+  //Monitor side nav
+  $('body').scrollspy({
+    target: '.bs-docs-sidebar',
+    offset: 160
+  });
+
+  $('#template-to-top').hide();
+  $(".bs-docs-sidebar").hide();
+
+  function checkVisible(elm) {
+    var rect = elm.getBoundingClientRect();
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+  }
+
+
+  var allLargeImgs = document.getElementsByClassName('full-screen-img');
+
+  //Check to see if the window is top/display button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $('#template-to-top').fadeIn();
+    } else {
+      $('#template-to-top').fadeOut();
+    }
+    if ($(this).scrollTop() > 600) {
+      var hasLargeImg = false;
+      Array.prototype.forEach.call(allLargeImgs, function(el) {
+          if (checkVisible(el)) {
+            hasLargeImg = true;
+          }
+      });
+      if (hasLargeImg) {
+        $(".bs-docs-sidebar").fadeOut('slow');
+      } else {
+        $(".bs-docs-sidebar").fadeIn('slow');
+      }
+    }
+    else {
+        $(".bs-docs-sidebar").fadeOut("slow");
+    }
+  });
+
+  $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 700, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+  //Click event to scroll to top
+    $('#template-to-top').click(function () {
+      $('html, body').animate({ scrollTop: 0 }, 600);
+      return false;
+    });    
+
+});
+
+//Highlight on screen
+$.fn.isOnScreenHighlight = function(){
+  var win = $(window);
+
+  var viewport = {
+      top : win.scrollTop(),
+      left : win.scrollLeft()
+  };
+  viewport.right = viewport.left + win.width();
+  viewport.bottom = viewport.top + win.height();
+
+  var bounds = this.offset();
+  bounds.right = bounds.left + this.outerWidth();
+  bounds.bottom = bounds.top + this.outerHeight();
+
+  return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.bottom || viewport.top > bounds.bottom));
+      
+};
